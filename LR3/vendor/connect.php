163e125ed17f4228cf8a4a6cbfa84669ede6f6fa
denissukhanov <99083218+denissukhanov@users.noreply.github.com>
2022-02-20 23:01:59 +0300

    Delete LR3 directory

--- LR3/vendor/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $connect = mysqli_connect('localhost', 'root', '', 'users');
-
-    if (!$connect) {
-        die('Error connect to DataBase');//если в переменной конект есть ошибка то выводится сообщение
-    }
